951dd478faa947df1d7c50d308edf808410a91c7
rokenshi <rouronikenshi@gmail.com>
2023-02-07 01:41:13 +0300

    bot + mariaDb

---
+++ src/main/java/jarvice/quiz/MedicineQuizBot/model/UserRepository.java
@@ -0,0 +1,7 @@
+package jarvice.quiz.MedicineQuizBot.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
--- src/main/java/jarvice/quiz/MedicineQuizBot/service/TelegramBot.java
+++ src/main/java/jarvice/quiz/MedicineQuizBot/service/TelegramBot.java
@@ -2,22 +2,49 @@ package jarvice.quiz.MedicineQuizBot.service;
 
 
 import jarvice.quiz.MedicineQuizBot.config.BotCongif;
+import jarvice.quiz.MedicineQuizBot.model.User;
+import jarvice.quiz.MedicineQuizBot.model.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+    static final String HELP_TEXT = "This bot helps you learn English, click /start to memorize words";
     final BotCongif congif;
 
     public TelegramBot(BotCongif congif){
         this.congif = congif;
+        List<BotCommand> botCommandList = new ArrayList<>();
+        botCommandList.add(new BotCommand("/start","get a welcome message"));
+        botCommandList.add(new BotCommand("/mydata", "get your data stored"));
+        botCommandList.add(new BotCommand("/deletedata","delete my data"));
+        botCommandList.add(new BotCommand("/help","info how to use this bot"));
+        botCommandList.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e){
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -40,12 +67,35 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
+
+                    registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
                 default:
+                    sendMessage(chatId, "sorry, command was not recognized");
+
 
 
             }
+        }
+    }
+
+    private void registerUser(Message msg) {
+        if(userRepository.findById(msg.getChatId()).isEmpty()){
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(String.valueOf(new Timestamp(System.currentTimeMillis())));
+
+            userRepository.save(user);
         }
     }
 
